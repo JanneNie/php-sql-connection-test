f655bedde943f58f5bcd985febc9942455e7758c
JanneNie <56626503+JanneNie@users.noreply.github.com>
2023-04-03 10:24:44 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,6 +3,17 @@ $servername = "web-app-database-test-server.mysql.database.azure.com";
 $username = "qnlnwbfheg";
 $password = "2B70E8OHL7FIJBM3$";
 
+
+mysqli_ssl_set(
+    mysqli $mysql,
+    ?string $key,
+    ?string $certificate,
+    ?string $ca_certificate,
+    ?string $ca_path,
+    ?string $cipher_algos
+): bool
+  
+  
 // Create connection
 $conn = new mysqli($servername, $username, $password);
 
